43bbc78a36f46cf469e2d4adc0fc79af3f11f805
Gaye Bulut <gayegul@gmail.com>
2016-05-02 00:18:04 -0700

    update item routes

--- routes/item_routes.js
+++ routes/item_routes.js
@@ -5,7 +5,7 @@ var itemRoutes = express.Router();
 
 module.exports = itemRoutes;
 
-itemRoutes.route('/items')
+itemRoutes.route('/users/:username/items')
   // returns all items for a given user
   .get(function(req, res) {
     Item.find({}, function(err, data) {
@@ -17,14 +17,14 @@ itemRoutes.route('/items')
   .post(function(req, res) {
     var item = new Item(req.body);
 
-    Item.findOne({ 'username': req.body.username, 'name': req.body.name }, function(err, item) {
+    Item.findOne({ 'username': req.params.username, 'itemname': req.params.itemname }, function(err, item) {
       if(err) return res.send(err);
-      if(item) return res.status(422).json({ msg: 'duplicate task' });
+      if(item) return res.status(422).json({ msg: 'Duplicate task' });
     });
 
-    User.findOne({ 'username': req.body.username }, function(err, user) {
+    User.findOne({ 'username': req.params.username }, function(err, user) {
       if(err) return res.send(err);
-      if(!user) return res.status(404).json({ msg: 'no such user' });
+      if(!user) return res.status(404).json({ msg: 'No such user' });
       item.save(function(err, data) {
         if(err) return res.send(err);
         res.status(200).json(data);
@@ -32,22 +32,22 @@ itemRoutes.route('/items')
     });
   });
 
-itemRoutes.route('/items/:name')
+itemRoutes.route('/users/:username/items/:itemname')
   // returns specific item
   .get(function(req, res) {
-    Item.findOne({ 'name': req.params.name }, function(err, item) {
+    Item.findOne({ 'itemname': req.params.itemname }, function(err, item) {
       if(err) return res.send(err);
-      if(!item) return res.status(404).json({ msg: 'no such item' });
+      if(!item) return res.status(404).json({ msg: 'No such item' });
       res.status(200).json(item);
     });
   })
   // updates item
   .put(function(req, res) {
-    Item.findOne({ 'name': req.params.name, 'username': req.body.username }, function(err, item) {
+    Item.findOne({ 'itemname': req.params.itemname, 'username': req.params.username }, function(err, item) {
       if(err) return res.send(err);
-      if(!item) return res.status(404).json({ msg: 'no such item' });
-      if(item.name === req.body.name) return res.json({ msg: 'same as previous info' });
-      if(item.name !== req.body.name) item.name = req.body.name;
+      if(!item) return res.status(404).json({ msg: 'No such item' });
+      if(item.itemname === req.body.itemname) return res.json({ msg: 'Same as previous info' });
+      if(item.itemname !== req.body.itemname) item.itemname = req.body.itemname;
       item.save(function(err, data) {
         if(err) return res.send(err);
         res.status(200).json(data);
@@ -56,12 +56,12 @@ itemRoutes.route('/items/:name')
   })
   // deletes item
   .delete(function(req, res) {
-    Item.findOne({ 'name': req.params.name }, function(err, item) {
+    Item.findOne({ 'itemname': req.params.itemname, 'username': req.params.username }, function(err, item) {
       if(err) return res.send(err);
-      if(!item) return res.status(404).json({ msg: 'no such item' });
+      if(!item) return res.status(404).json({ msg: 'No such item' });
       item.remove(function(err) {
         if(err) return res.send(err);
-        res.status(200).json({ msg: 'success' });
+        res.status(200).json({ msg: 'Success' });
       });
     });
   });
