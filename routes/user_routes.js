5a4fd2bb0b2cf2d1b302c259bdcc643f7884727d
Gaye Bulut <gayegul@gmail.com>
2016-05-11 00:24:34 -0700

    update user routes

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,30 +1,11 @@
 var express = require('express');
 var User = require('../models/user');
 var userRouter = express.Router();
+var authRouter = require('./auth_routes');
+var jwt = require('jsonwebtoken');
+var superSecret = 'ilovemakeupnotsosecretbutyeah';
 
 module.exports = userRouter;
-
-userRouter.route('/admin/users')
-  // returns all users for admin
-  .get(function(req, res) {
-    User.find({}, function(err, data) {
-      if(err) return res.send(err);
-      res.status(200).json(data);
-    });
-  });
-
-userRouter.route('/admin/users/:username')
-  // deletes a specific user for admin
-  .delete(function(req, res) {
-    User.findOne({ 'username': req.params.username }, function(err, user) {
-      if(err) return res.send(err);
-      if(!user) return res.status(404).json({ msg: 'User does not exist' });
-      User.remove({ 'username': req.params.username }, function(err) {
-        if(err) return res.send(err);
-        res.status(200).json({ msg: 'Success' });
-      });
-    });
-  });
 
 userRouter.route('/users/signup')
   // creates a user
@@ -34,14 +15,43 @@ userRouter.route('/users/signup')
       if(data) return res.status(422).json({ msg: 'User already exists' });
       var user = new User();
       user.username = req.body.username;
-      user.authentication.email = req.body.email;
-      user.authentication.password = req.body.password;
+      user.email = req.body.email;
+      user.password = req.body.password;
       user.save(function(err, data) {
         if(err) return res.send(err);
         res.status(200).json(data);
       });
     });
   });
+
+// middleware to verify a token
+userRouter.use(function(req, res, next) {
+  // check header or url params or post params for token
+  var token = req.body.token || req.query.token || req.headers['token'];
+
+  // decode token
+  if(token) {
+    // verifies secret and checks expiration
+    jwt.verify(token, superSecret, function(err, decoded) {
+      if(err) {
+        return res.status(403).send({
+          success: false,
+          msg: 'Failed to authenticate token.'
+        });
+      } else {
+        // if everything checks out, save to req to use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token, return HTTP res of 403 (access forbidden)
+    return res.status(403).send({
+      success: false,
+      msg: 'No token provided'
+    });
+  }
+});
 
 userRouter.route('/users/:username')
   // returns a specific user
@@ -55,11 +65,19 @@ userRouter.route('/users/:username')
   .put(function(req, res) {
     User.findOne({ 'username': req.params.username }, function(err, user) {
       if(err) return res.send(err);
-      if(!user) return res.status(404).json({ msg: 'No such user' });
-      if(user.username === req.body.username && user.authentication.email === req.body.email) return res.status(422).json({ msg: 'No new info' });
-      if(user.username !== req.body.username) user.username = req.body.username;
-      if(user.authentication.email !== req.body.email) user.authentication.email = req.body.email;
-      // if(user.authentication.password !== req.body.password) user.authentication.password = req.body.password;
+      else if(!user) return res.status(404).json({ msg: 'No such user' });
+      else if(req.body.email) {
+        if(user.username === req.body.username && user.email === req.body.email) {
+          return res.status(422).json({ msg: 'No new info' });
+        }
+        else {
+          user.username = req.body.username;
+          user.email = req.body.email;
+        }
+      }
+      else if(user.username === req.body.username) return res.status(422).json({ msg: 'No new info' });
+      else if(user.username !== req.body.username) user.username = req.body.username;
+
       user.save(function(err, data) {
         if(err) return res.send(err);
         res.status(200).json(data);
@@ -70,7 +88,7 @@ userRouter.route('/users/:username')
   .delete(function(req, res) {
     User.findOne({ 'username': req.params.username }, function(err, user) {
       if(err) return res.send(err);
-      if(!user) return res.status(404).json({ msg: 'You cannot delete someone that is not there' });
+      else if(!user) return res.status(404).json({ msg: 'You cannot delete someone that is not there' });
       User.remove({ 'username': req.params.username }, function(err) {
         if(err) return res.send(err);
         res.status(200).json({ msg: 'Success' });
